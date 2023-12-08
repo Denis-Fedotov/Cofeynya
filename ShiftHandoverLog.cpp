aca4747aba188c895810e7b590e6ead85bf76507
Denis Fedotov <asenter88@mail.ru>
2023-12-08 21:39:53 +0300

    Implemented ShiftHandoverLog

--- ShiftHandoverLog.cpp
+++ ShiftHandoverLog.cpp
@@ -9,26 +9,38 @@
 
 void ShiftHandoverLog::ShowAlcoholList()
 {
-	std::cout<<"TODO";
+	for(int i=0; i<listOfAlcohol.size(); i++)
+	{
+		listOfAlcohol[i]->show();
+	}
 	return;
 }
 void ShiftHandoverLog::FillLog(User* fillingUser, TrackedProdList* pList)
 {
-	std::cout<<"TODO";
+	NameOfBarman = fillingUser->GetName();
+	std::cout<<std::endl<<"Please enter today's date: ";
+	std::cin>>Date;
+	pList->getChanged(&ListOfProduct);
 	return;
 }
 void ShiftHandoverLog::OpenAlcohol()
 {
-	std::cout<<"TODO";
+	Alcohol* temp = new Alcohol;
+	temp->fillRecord();
+	listOfAlcohol.push_back(temp);
 	return;
 }
 void ShiftHandoverLog::Show()
 {
-	std::cout<<"TODO";
+	std::cout<<std::endl<<"Name: "<<NameOfBarman<<std::endl<<"  Date: "<<Date;
 	return;
 }
 void ShiftHandoverLog::showProductList()
 {
-	std::cout<<"TODO";
+	for(int i=0; i<ListOfProduct.size(); i++)
+	{
+		std::cout<<"1. ";
+		ListOfProduct[i]->show();
+	}
 	return;
 }
