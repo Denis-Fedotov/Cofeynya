f9d06419cdbe25543f317ea13926089981c321d8
VeGnError <arsreplay@rambler.ru>
2023-12-14 13:34:00 +0300

    modifed ShowReports()

--- ReportList.cpp
+++ ReportList.cpp
@@ -3,18 +3,29 @@
 #include <vector>
 #include "Report.h"
 #include "ReportList.h"
-using namespace std;
+
 void ReportList::ShowReports()
 {
+    int inp;
     for (int i = 0; i < ListOfReports.size(); i++){
-        cout<<i+1 <<" Report"<<endl;
-        cout<<ListOfReports[i]<<endl;
+        std::cout<<i+1 <<". Report"<<std::endl;
+        ListOfReports[i]->show();
     }
-        return;
+    std::cout<<std::endl<<"Please choose the report to see details: ";
+    std::cin>>inp;
+    std::cin.ignore(1,'\n');
+    if(inp<=ListOfReports.size() && inp>0)
+    {
+            std::cout<<std::endl<<"Report is: "<<std::endl;
+            ListOfReports[inp-1]->SeeInvoiceList();
+            return;
+
+    }
+    else return;
 }
 void ReportList::addReport(Report* targRep)
 {
         ListOfReports.push_back(targRep);
-        cout<<"Report added"<<endl;
+        std::cout<<"Report added"<<std::endl;
 	return;
 }
