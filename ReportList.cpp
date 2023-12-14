d052cd5ac827fe51fc859961989e2af989a32a39
VeGnError <arsreplay@rambler.ru>
2023-12-14 12:38:43 +0300

    add void addReport

--- ReportList.cpp
+++ ReportList.cpp
@@ -3,14 +3,14 @@
 #include <vector>
 #include "Report.h"
 #include "ReportList.h"
-
+using namespace std;
 void ReportList::ShowReports()
 {
-	std::cout<<"TODO";
-	return;
+
 }
 void ReportList::addReport(Report* targRep)
 {
-	std::cout<<"TODO";
+        ListOfReports.push_back(targRep);
+        cout<<"Report added"<<endl;
 	return;
 }
